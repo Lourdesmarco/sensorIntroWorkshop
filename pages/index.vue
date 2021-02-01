d46f28d002120ed6cb1009410c65334ad247cba2
Lourdes <lourdesmarco.design@gmail.com>
2021-02-01 16:46:22 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+    <main @click="count += 1">          
+      <h1 class="no-select" :style="cssVars">Sensor<br>Variable<br>Font<br></h1>
+      <h2 class="no-select">Workshop</h2>     
+      <p>{{ count }}</p> 
+    </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--BACK': this.count,        
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+:root{
+    /*From 1 to 900*/
+    --BACK: 1;
+}
+
+@font-face {
+    font-family: "Grid";
+    src: url("../assets/font/GridlitePEVFTRIAL-All.ttf") format("truetype-variations");
+}
+
+html, body{
+    height: 100%;
+    margin: 0;
+    padding: 0;  
+}
+#__nuxt, #__layout{
+  height: 100%;
+}
+
+.no-select {
+  -webkit-user-select: none;  /* Chrome all / Safari all */
+  -moz-user-select: none;     /* Firefox all */
+  -ms-user-select: none;      /* IE 10+ */
+  user-select: none;          /* Likely future */      
+}
+main{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    animation-name: backgroundColorPalette;
+    animation-duration: 8s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+}
+h1{
+    margin: 0;
+    font-family: "Grid";
+    font-variation-settings: "wght" 1, "BACK" var(--BACK), "ESHP" 3;
+    color: #00AEEF;
+    font-size: 10em;
+    text-align: center;
+
+}
+h2{
+    font-family: "Grid";
+    font-variation-settings: "wght" 623, "BACK" 140, "ESHP" 4;
+}
+
+@keyframes backgroundColorPalette {
+    0% {
+      background: #00AEEF;
+    }
+    25% {
+      background: #2D78FD;
+    }
+    50% {
+      background: #5A59FB;
+    }
+    75% {
+      background: #B084F8;
+    }
+    100% {
+      background: #E4AEF6;
+    }
+  }
+
+
+</style>
